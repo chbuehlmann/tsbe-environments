a7da4c54941b028bdf40ee0cd8a25f6c4520aa93
Christoph Buehlmann <chrigu.b@gmail.com>
2018-01-20 15:21:35 +0100

    setzte pve_modules_file_path explizit

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,11 @@
 node default { 
 
   include nano
-  include proxmox4::hypervisor
+
+  class { 'proxmox4::hypervisor':
+    pveproxy_allow    => '127.0.0.1,192.168.1.0/24',
+    cluster_name      => 'TSBE',
+	pve_modules_file_path	=> '/etc/modules-load.d/proxmox.conf'
+  }
 
 }
