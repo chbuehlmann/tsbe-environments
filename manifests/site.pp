5baaaf2f03333bbe53468eca75def5582ee4083b
Christoph Buehlmann <chrigu.b@gmail.com>
2018-01-20 15:03:29 +0100

    proxmox

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,6 @@
 node default { 
 
   include nano
-
-  class { 'proxmox4::hypervisor':
-    pveproxy_allow    => '127.0.0.1,192.168.1.0/24',
-    cluster_name      => 'TSBE',
-  }
+  include proxmox4::hypervisor
 
 }
